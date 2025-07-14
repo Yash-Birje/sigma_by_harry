2f6b80c29a5f14089b66184b29db7ef727203c9f
plz-help-me-pro <yashbirje21@gmail.com>
2025-07-14 23:16:58 +0530

    djd4

--- django/textutils/textutils/views.py
+++ django/textutils/textutils/views.py
@@ -1,7 +1,24 @@
 #I have created this file - Yash
 from django.http import HttpResponse
+from django.shortcuts import render
 def index(request):
-    return HttpResponse("Hello, world. You're at the textutils index.")
+    params = {'name': 'Yash', 'place': 'India'}
+    return render(request, 'index.html', params)
 
 def about(request):
-    return HttpResponse("This is the about page for the textutils app.")
+    return render(request, 'about.html')
+
+def remove_punctuation(request):
+    return render(request, 'remove-punc.html')
+
+def capitalize(request):
+    return render(request, 'capitalize.html')
+
+def space_remover(request):
+    return render(request, 'space-remover.html')
+
+def char_count(request):
+    return render(request, 'charcount.html')
+
+def line_remover(request):
+    return render(request, 'lineremover.html')
