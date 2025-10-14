aefe6ee5d0f355e54d7a2ad3404aaea8c56d74fd
Yash Birje <yashbirje21@gmail.com>
2025-10-15 01:04:06 +0530

    updated path from form.js to server to form to submit.js

--- frontend/projects/porfolio_static/form.js
+++ frontend/projects/porfolio_static/form.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async (e) => {
   const data = Object.fromEntries(formData);
   console.log('Form Data:', data);
   // send to backend
-  const response = await fetch("/submit", {
+  const response = await fetch("/api/submit", {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
